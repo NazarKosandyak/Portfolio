dedff830090d61ac38f0fa9ad5a0aaef82b83e47
Nazar <nazarkosandyak@gmail.com>
2021-07-23 16:04:46 +0300

    '2.0v'

---
+++ main.js
@@ -0,0 +1,8 @@
+let mySwiper  = document.querySelector('.mySwiper ')
+let one = mySwiper.children[3]
+let mainTarget = one.children
+
+for (const iterator of mainTarget) iterator.style.background = '#06FF6B'
+
+  
+
