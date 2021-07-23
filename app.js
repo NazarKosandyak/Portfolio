f44d11b4f3d3fc67a674de837eb4656ecf0e74aa
Nazar <nazarkosandyak@gmail.com>
2021-07-23 16:50:25 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// app.use(express.static('/dist/AnnouncementWebsite'));
-// app.get('/*',(req,res)=>{'index.html',{root:'dist/AnnouncementWebsite/'}});
-// app.listen(port)
-
-
 app.use(express.static('./public'));
 app.get('/*', (req, res) =>
       res.sendFile('index.html', {root: 'public/'}),
