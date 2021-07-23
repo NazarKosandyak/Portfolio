e39b52123ce52bfc620f97238254945b67dda44a
Nazar <nazarkosandyak@gmail.com>
2021-07-23 16:48:32 +0300

    fixed

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// app.use(express.static('./Portfolio'));
-// app.get('/*', (req, res) =>
-//       res.sendFile('index.html', {root: 'Portfolio/'}),
-// );
-// app.listen(process.env.PORT || 5000);
+// app.use(express.static('/dist/AnnouncementWebsite'));
+// app.get('/*',(req,res)=>{'index.html',{root:'dist/AnnouncementWebsite/'}});
+// app.listen(port)
+
+
+app.use(express.static('./public'));
+app.get('/*', (req, res) =>
+      res.sendFile('index.html', {root: 'public/'}),
+);
+app.listen(process.env.PORT || 4000);
